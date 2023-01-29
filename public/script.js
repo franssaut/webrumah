b74ae1d7433d2733c94027320274d06b4ab680bb
franssaut <franssaut@gmail.com>
2023-01-29 17:12:58 +0700

    first commit

---
+++ public/script.js
@@ -0,0 +1,16 @@
+let warna1 = "";
+
+
+
+var title =document.getElementsByClassName("title");
+// title.style.backgroundColor="red";
+console.log(title)
+for(var i=0; i<document.title.length;i++){
+   let name= title[i].innerText;
+   
+   console.log(name)
+    
+}
+
+
+
